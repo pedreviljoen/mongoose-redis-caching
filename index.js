5b869ed1de9cd279db29db9035382c928372e7ec
Pedré Viljoen <pedreviljoen@ffmans-MacBook-Pro.local>
2018-12-20 13:37:16 +0200

    Init commit :beers:

---
+++ index.js
@@ -0,0 +1,50 @@
+const redis     = require('redis')
+const util      = require('util')
+const redisUrl  = process.env.REDIS_URL || 'redis://127.0.0.1:6379'
+const client    = redis.createClient(redisUrl)
+client.get      = util.promisify(client.get)
+
+module.exports = function (mongoose) {
+    const exec = mongoose.Query.prototype.exec
+
+    mongoose.Query.prototype.cache = function () {
+        this._cache = true
+        return this
+    }
+    
+    mongoose.Query.prototype.exec = async function () {
+        console.log(`[QUERY]: ${this.getQuery()}`)
+        console.log(`[COL]: ${this.mongooseClient.name}`)
+    
+        if (!this._cache) {
+            return exec.apply(this, arguments)
+        }
+    
+        const key = JSON.stringify(
+            Object.assign({}, this.getQuery(), {
+                collection: this.mongooseClient.name
+            })
+        )
+    
+        const cacheValue = await client.get(key)
+        if (cacheValue) {
+            const doc = JSON.parse(cacheValue)
+    
+            return Array.isArray(doc) 
+                ? (doc.map(d => new this.model(d))) 
+                : new this.model(doc)
+        }
+    
+        const result = await exec.apply(this, arguments)
+        client.set(key, JSON.stringify(result), 'EX', 10)
+        return result
+    }
+}
+
+
+// const mongoose = require('mongoose')
+// const mongooseRedisCaching = require('mongoose-redis-caching')
+
+// mongooseRedisCaching(mongoose)
+
+
